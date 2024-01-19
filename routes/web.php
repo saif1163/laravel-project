1268acd7c78d641928fc4bf4252def36936848a6
Your Name <your.email@example.com>
2024-01-19 01:14:39 -0800

    Laravel_Auth

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [AuthController::class, 'login_view'])->name('login');
+Route::post('/', [AuthController::class, 'login_process']);
+Route::get('/reg', [AuthController::class, 'reg_view'])->name('reg');
+Route::post('/reg', [AuthController::class, 'reg_process']);
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/dashboard', [AuthController::class, 'dashboard', 'middleware'=> 'auth'])->name('home');
+
